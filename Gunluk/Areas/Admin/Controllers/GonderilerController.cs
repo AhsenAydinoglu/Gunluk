1f4de74ccca271b6de1180694b88a9e86c060465
AhsenAydinoglu <ahsenaydinoglu@gmail.com>
2023-01-11 00:31:55 +0300

    gonderi düzenle ve sil

--- Gunluk/Areas/Admin/Controllers/GonderilerController.cs
+++ Gunluk/Areas/Admin/Controllers/GonderilerController.cs
@@ -53,7 +53,54 @@ namespace Gunluk.Areas.Admin.Controllers
         }
         public IActionResult Duzenle(int id)
         {
+            var gonderi = _db.Gonderiler.Find(id);
+            if (gonderi == null)
+                return NotFound();
+
+            var vm = new GonderiViewModel()
+            {
+                Baslik = gonderi.Baslik,
+                Icerik = gonderi.Icerik,
+                Id = gonderi.Id,
+                KategoriId = gonderi.KategoriId,
+            };
+
+            KategorileriYukle();
+
+            return View("Yonet", vm);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Duzenle(GonderiViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var gonderi = _db.Gonderiler.Find(vm.Id);
+                if (gonderi == null)
+                    return NotFound();
+
+                gonderi.Baslik = vm.Baslik;
+                gonderi.Icerik = vm.Icerik;
+                gonderi.DegistirilmeZamani = DateTime.Now;
+                gonderi.KategoriId = vm.KategoriId!.Value;
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index), new { durum = "duzenlendi" });
+            }
+            KategorileriYukle();
             return View("Yonet");
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Sil(int id)
+        {
+            var gonderi = _db.Gonderiler.Find(id);
+            if (gonderi == null)
+                return NotFound();
+
+            _db.Gonderiler.Remove(gonderi);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index), new { durum = "silindi" });
+
+            
         }
     }
 }
